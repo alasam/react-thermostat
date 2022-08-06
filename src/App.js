bc50c6f4683efd963b1543f444a28cafedda55da
Anthony <alasam@live.com>
2022-08-05 22:25:56 -0400

    change colour for temperature Colour, add dynamic temperatureColour in class

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 
 const App = () => {
   // state for temperature value
-  const [temperatureValue, setTemperatureValue] = useState(0);
+  const [temperatureValue, setTemperatureValue] = useState(21);
   // state for temperature colour
-  const [temparatureColour, setTemperatureColour] = useState();
+  const [temperatureColour, setTemperatureColour] = useState("neutral");
   
   return (
     // box containing thermometer
@@ -12,7 +12,7 @@ const App = () => {
       {/* circle containing temperature value */}
       <div className="temperature-display-container">
         {/* temperature value */}
-        <div className="temperature-display">{temperatureValue}℃</div>
+        <div className={`temperature-display ${temperatureColour}`}>{temperatureValue}℃</div>
       </div>
       {/* container for buttons to adjust temperature */}
       <div className="button-container">
