0d11f809cb15eaf1f5fe1c49f82a4d62d3cc9d0c
Anthony <alasam@live.com>
2022-08-05 21:55:44 -0400

    onClick events for buttons to increase/decrease temperature value, add temperature Colour states, and add comments

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [temperatureValue, setTemperatureValue] = useState();
+  // state for temperature value
+  const [temperatureValue, setTemperatureValue] = useState(0);
+  // state for temperature colour
+  const [temparatureColour, setTemperatureColour] = useState();
+  
   return (
+    // box containing thermometer
     <div className="app-container">
+      {/* circle containing temperature value */}
       <div className="temperature-display-container">
+        {/* temperature value */}
         <div className="temperature-display">{temperatureValue}℃</div>
       </div>
+      {/* container for buttons to adjust temperature */}
       <div className="button-container">
-        <button>⇧</button>
-        <button>⇩</button>
+        {/* buttons */}
+        <button onClick={() => setTemperatureValue(temperatureValue + 1)}>⇧</button>
+        <button onClick={() => setTemperatureValue(temperatureValue - 1)}>⇩</button>
       </div>
     </div>
   );
